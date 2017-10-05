0b1996bb810ffc895de0316a584dda8d25473821
Briana Sanchez <bsanchez@tkgdl-145.tacitknowledge.com>
2017-10-05 14:24:23 -0500

    Working on authentication

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # pgApiQuery
+
+DESCRIPTION
+
+This script's purpose is to perform querys/searches into the PagerDuty Api by providing searching parameters to narrow down the seach and make it more specific. 
+
+INSTALL
+
+This script is working with python 3. 
+For the script to work properly it needs to have installed the module 'requests' and 'requests.Session'. To install the them run the following:
+
+	pip3 install requests
+	pip3 install requests.Session
--- pgApiQuery.py
+++ pgApiQuery.py
@@ -1,1 +1,28 @@
-#!/usr/local/bin/python
+#!/usr/local/bin/python3
+
+# MODULES
+
+import requests
+
+# VARIABLES
+
+domain = input("Introduce your company domain: ")
+authorization_token = input("Introduce your authentication token: ")
+
+# AUTHENTICATION TO THE API
+pagerduty_session = requests.Session()
+pagerduty_session.headers.update({'Authorization': 'Token token=' + authorization_token, 'Accept': 'application/vnd.pagerduty=json;version=2'})
+pagerduty_session.get('https://{domain}.pagerduty.com/users')
+
+# ANOTHER AUTHENTICATION METHOD
+
+headers = { 'Authorization': 'Token token={authorization_token}' }
+response = requests.get('https://{domain}.pagerduty.com/api/v1/incidents'), headers=headers)
+print response.status_code
+#pg_api_session = pagerduty_session.get('https://{domain}.pagerduty.com/api/v1/incidents')
+#print(pg_api_session.text)
+
+# SEARCH PARAMETERS
+
+#response = requests.get("https://{domain}.pagerduty.com/api/v1/incidents")
+#print(response.status_code)
